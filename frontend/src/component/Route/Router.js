42e252b3f9c21ce61d39418ad45aaeedb078bc1f
unknown <kingforworkonly0201@gmail.com>
2021-12-06 09:36:17 -0500

    update

--- frontend/src/component/Route/Router.js
+++ frontend/src/component/Route/Router.js
@@ -2,7 +2,7 @@ import React from "react";
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import Home from "../Home/Home";
 import UserGet from '../User/UserGet';
-import UserPost from '../User/UserPost';
+import UserPost from '../User/UserRegistration';
 
 function Navigation(){
     return(
@@ -11,12 +11,15 @@ function Navigation(){
                 <Link to='/'>Main</Link>
                 <Link to='/home'>Home</Link>
                 <Link to='/user'>User</Link>
+                <Link to='/register'>Registration</Link>
                 <Switch>
                     <Route path='/home'>
                         <Home/>
                     </Route>
                     <Route path='/user'>
                         <UserGet/>
+                    </Route>
+                    <Route path='/register'>
                         <UserPost/>
                     </Route>
                 </Switch>
